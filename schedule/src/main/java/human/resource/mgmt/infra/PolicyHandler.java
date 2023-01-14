a62c31ce9400be3a79e6db8d0e90bfc2e09693a1
Jinyoung Jang <jyjang@uengine.org>
2023-01-14 23:14:41 +0900

    commit

--- schedule/src/main/java/human/resource/mgmt/infra/PolicyHandler.java
+++ schedule/src/main/java/human/resource/mgmt/infra/PolicyHandler.java
@@ -5,36 +5,63 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import human.resource.mgmt.config.kafka.KafkaProcessor;
 import human.resource.mgmt.domain.*;
 import javax.naming.NameParser;
-import javax.naming.NameParser;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public class PolicyHandler {
 
     @Autowired
     CalendarRepository calendarRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void whatever(@Payload String eventString) {}
+    public void whatever(
+        @Payload String eventString,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey
+    ) {
+        /*
+          // Call port method with received messageKey to publish msg to the same partition. //
+          DomainClass.portMethod(eventString, new String(messageKey));
+          
+          // ,or //
+          new EventRaised(domain Obj).publishAfterCommit(new String(messageKey));
+          // manual Offset Commit. //
+          acknowledgment.acknowledge();  
+          */
+    }
 
     @StreamListener(
         value = KafkaProcessor.INPUT,
         condition = "headers['type']=='VacationRegistered'"
     )
     public void wheneverVacationRegistered_AddCalendar(
-        @Payload VacationRegistered vacationRegistered
+        @Payload VacationRegistered vacationRegistered,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey
     ) {
         VacationRegistered event = vacationRegistered;
         System.out.println(
             "\n\n##### listener AddCalendar : " + vacationRegistered + "\n\n"
         );
-        // Sample Logic //
 
+        AddCalendarCommand AddCalendarCommand = new AddCalendarCommand();
+
+        calendarRepository
+            .findById(event.getId())
+            .ifPresent(aggregate -> {
+                aggregate.addCalendar(AddCalendarCommand);
+            });
+
+        // Manual Offset Commit //
+        acknowledgment.acknowledge();
     }
 
     @StreamListener(
@@ -42,14 +69,25 @@ public class PolicyHandler {
         condition = "headers['type']=='VacationCancelled'"
     )
     public void wheneverVacationCancelled_CancelCalendar(
-        @Payload VacationCancelled vacationCancelled
+        @Payload VacationCancelled vacationCancelled,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey
     ) {
         VacationCancelled event = vacationCancelled;
         System.out.println(
             "\n\n##### listener CancelCalendar : " + vacationCancelled + "\n\n"
         );
-        // Sample Logic //
 
+        CancelCalendarCommand CancelCalendarCommand = new CancelCalendarCommand();
+
+        calendarRepository
+            .findById(event.getId())
+            .ifPresent(aggregate -> {
+                aggregate.cancelCalendar(CancelCalendarCommand);
+            });
+
+        // Manual Offset Commit //
+        acknowledgment.acknowledge();
     }
 
     @StreamListener(
@@ -57,14 +95,25 @@ public class PolicyHandler {
         condition = "headers['type']=='VacationRejected'"
     )
     public void wheneverVacationRejected_CancelCalendar(
-        @Payload VacationRejected vacationRejected
+        @Payload VacationRejected vacationRejected,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey
     ) {
         VacationRejected event = vacationRejected;
         System.out.println(
             "\n\n##### listener CancelCalendar : " + vacationRejected + "\n\n"
         );
-        // Sample Logic //
 
+        CancelCalendarCommand CancelCalendarCommand = new CancelCalendarCommand();
+
+        calendarRepository
+            .findById(event.getId())
+            .ifPresent(aggregate -> {
+                aggregate.cancelCalendar(CancelCalendarCommand);
+            });
+
+        // Manual Offset Commit //
+        acknowledgment.acknowledge();
     }
 
     @StreamListener(
@@ -72,13 +121,24 @@ public class PolicyHandler {
         condition = "headers['type']=='EmployeeJoined'"
     )
     public void wheneverEmployeeJoined_RegisterCalendar(
-        @Payload EmployeeJoined employeeJoined
+        @Payload EmployeeJoined employeeJoined,
+        @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey
     ) {
         EmployeeJoined event = employeeJoined;
         System.out.println(
             "\n\n##### listener RegisterCalendar : " + employeeJoined + "\n\n"
         );
-        // Sample Logic //
 
+        RegisterCalendarCommand RegisterCalendarCommand = new RegisterCalendarCommand();
+
+        calendarRepository
+            .findById(event.getUserId())
+            .ifPresent(aggregate -> {
+                aggregate.registerCalendar(RegisterCalendarCommand);
+            });
+
+        // Manual Offset Commit //
+        acknowledgment.acknowledge();
     }
 }
